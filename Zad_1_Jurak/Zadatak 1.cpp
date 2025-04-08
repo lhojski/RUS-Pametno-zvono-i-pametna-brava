c8ceec7613e57acb9d69f90906c4d26d4397ddc0
DjurakTVZ <djurak@tvz.hr>
2025-04-08 20:46:37 +0200

    Update Zadatak 1.cpp

--- Zad_1_Jurak/Zadatak 1.cpp
+++ Zad_1_Jurak/Zadatak 1.cpp
@@ -1,25 +1,52 @@
+/**
+ * @file main.ino
+ * @brief Glavni program za upravljanje prekidima i potrošnjom energije
+ * @version 1.0
+ * @date 2023-12-15
+ */
+
 #include <Arduino.h>
 
-// Definiranje pinova
-const int buttonPins[] = {2, 3, 18};  // INT0, INT1, INT5
-const int ledPin = 13;
-const int potPin = A0;
+/** @defgroup Pinovi Definirani pinovi
+ *  @{
+ */
+const int buttonPins[] = {2, 3, 18};  ///< Pinovi za tipkala (INT0, INT1, INT5)
+const int ledPin = 13;                ///< Pin za LED diodu
+const int potPin = A0;                ///< Pin za potenciometar
+/** @} */
 
-// Globalne varijable
-volatile bool buttonPressed[] = {false, false, false};
-volatile int sensorValue = 0;
-volatile bool timerFlag = false;
-volatile bool ledState = false;
+/** @defgroup DebugPinovi Debug pinovi za logički analizator
+ *  @{
+ */
+const int debug_INT0 = 14;  ///< Debug pin za INT0 aktivnost
+const int debug_INT1 = 15;  ///< Debug pin za INT1 aktivnost
+const int debug_INT5 = 16;  ///< Debug pin za INT5 aktivnost
+const int debug_LED = 19;   ///< Debug pin za LED stanje
+const int debug_Timer = 20; ///< Debug pin za Timer događaje
+const int debug_Serial = 21;///< Debug pin za serijsku komunikaciju
+/** @} */
 
-// Semafori
-volatile bool serialInUse = false;
-volatile bool sensorInUse = false;
+/** @defgroup GlobalneVarijable Globalne varijable
+ *  @{
+ */
+volatile bool buttonPressed[] = {false, false, false}; ///< Stanje tipkala
+volatile int sensorValue = 0;         ///< Očitana vrijednost senzora
+volatile bool timerFlag = false;      ///< Zastavica za timer prekid
+volatile bool ledState = false;       ///< Trenutno stanje LED diode
+/** @} */
 
-// Prototipovi funkcija
-void handleButtonEvents();
-void handleSensorReading();
-void normalOperation();
+/** @defgroup Semafori Semafori za zaštitu resursa
+ *  @{
+ */
+volatile bool serialInUse = false;    ///< Zaštita serijske komunikacije
+volatile bool sensorInUse = false;    ///< Zaštita čitanja senzora
+/** @} */
 
+/**
+ * @brief Inicijalizacija sustava
+ * 
+ * Funkcija koja se poziva jednom pri pokretanju za postavljanje početnih vrijednosti
+ */
 void setup() {
   // Inicijalizacija serijske komunikacije
   Serial.begin(9600);
@@ -32,29 +59,48 @@ void setup() {
   for (int i = 0; i < 3; i++) {
     pinMode(buttonPins[i], INPUT_PULLUP);
   }
-  
-  // Konfiguracija eksternih prekida
+
+  // Inicijalizacija debug pinova
+  pinMode(debug_INT0, OUTPUT);
+  pinMode(debug_INT1, OUTPUT);
+  pinMode(debug_INT5, OUTPUT);
+  pinMode(debug_LED, OUTPUT);
+  pinMode(debug_Timer, OUTPUT);
+  pinMode(debug_Serial, OUTPUT);
+  digitalWrite(debug_INT0, LOW);
+  digitalWrite(debug_INT1, LOW);
+  digitalWrite(debug_INT5, LOW);
+  digitalWrite(debug_LED, LOW);
+  digitalWrite(debug_Timer, LOW);
+  digitalWrite(debug_Serial, LOW);
+
+  // Konfiguracija prekida
   attachInterrupt(digitalPinToInterrupt(buttonPins[0]), ISR_button0, FALLING);
   attachInterrupt(digitalPinToInterrupt(buttonPins[1]), ISR_button1, FALLING);
   attachInterrupt(digitalPinToInterrupt(buttonPins[2]), ISR_button5, FALLING);
-  
-  // Konfiguracija timer prekida (Timer1)
+
+  // Konfiguracija Timer1
   noInterrupts();
   TCCR1A = 0;
   TCCR1B = 0;
   TCNT1 = 0;
-  
   OCR1A = 15624; // 1Hz (16MHz/1024/1Hz - 1)
   TCCR1B |= (1 << WGM12); // CTC mod
-  TCCR1B |= (1 << CS12) | (1 << CS10); // 1024 prescaler
+  TCCR1B |= (1 << CS12) | (1 << CS10); // 1024 preskaler
   TIMSK1 |= (1 << OCIE1A); // Omogući timer compare interrupt
   interrupts();
   
   Serial.println("Sustav spreman");
 }
 
-// INT0 prekidna rutina (najviši prioritet)
+/**
+ * @brief Prekidna rutina za INT0 (najviši prioritet)
+ * 
+ * Funkcija koja se poziva pritiskom tipkala na pinu 2
+ * @note Vrijeme debouncea: 200ms
+ */
 void ISR_button0() {
+  digitalWrite(debug_INT0, HIGH); // Debug signal
   static unsigned long lastInterruptTime = 0;
   unsigned long interruptTime = millis();
   
@@ -62,17 +108,27 @@ void ISR_button0() {
     buttonPressed[0] = true;
     ledState = true;
     digitalWrite(ledPin, HIGH);
+    digitalWrite(debug_LED, HIGH);
     
-    // Simulacija kratke obrade
+    // Simulacija obrade
     for (volatile int i = 0; i < 500; i++);
     
     Serial.println("INT0 prekid - najviši prioritet");
+    digitalWrite(debug_Serial, HIGH);
+    delayMicroseconds(100);
+    digitalWrite(debug_Serial, LOW);
   }
   lastInterruptTime = interruptTime;
+  digitalWrite(debug_INT0, LOW);
 }
 
-// INT1 prekidna rutina (srednji prioritet)
+/**
+ * @brief Prekidna rutina za INT1 (srednji prioritet)
+ * 
+ * Funkcija koja se poziva pritiskom tipkala na pinu 3
+ */
 void ISR_button1() {
+  digitalWrite(debug_INT1, HIGH);
   static unsigned long lastInterruptTime = 0;
   unsigned long interruptTime = millis();
   
@@ -80,15 +136,21 @@ void ISR_button1() {
     buttonPressed[1] = true;
     ledState = true;
     digitalWrite(ledPin, HIGH);
+    digitalWrite(debug_LED, HIGH);
     
-    // Kratka obrada
     for (volatile int i = 0; i < 300; i++);
   }
   lastInterruptTime = interruptTime;
+  digitalWrite(debug_INT1, LOW);
 }
 
-// INT5 prekidna rutina (najniži prioritet)
+/**
+ * @brief Prekidna rutina za INT5 (najniži prioritet)
+ * 
+ * Funkcija koja se poziva pritiskom tipkala na pinu 18
+ */
 void ISR_button5() {
+  digitalWrite(debug_INT5, HIGH);
   static unsigned long lastInterruptTime = 0;
   unsigned long interruptTime = millis();
   
@@ -96,12 +158,19 @@ void ISR_button5() {
     buttonPressed[2] = true;
     ledState = true;
     digitalWrite(ledPin, HIGH);
+    digitalWrite(debug_LED, HIGH);
   }
   lastInterruptTime = interruptTime;
+  digitalWrite(debug_INT5, LOW);
 }
 
-// Timer1 interrupt rutina
+/**
+ * @brief Timer1 interrupt rutina (1Hz)
+ * 
+ * Očitava vrijednost potenciometra svakih 500ms
+ */
 ISR(TIMER1_COMPA_vect) {
+  digitalWrite(debug_Timer, HIGH);
   static unsigned long lastSensorRead = 0;
   unsigned long currentMillis = millis();
   
@@ -115,14 +184,23 @@ ISR(TIMER1_COMPA_vect) {
       timerFlag = true;
     }
   }
+  digitalWrite(debug_Timer, LOW);
 }
 
+/**
+ * @brief Glavna petlja programa
+ */
 void loop() {
   handleButtonEvents();
   handleSensorReading();
   normalOperation();
 }
 
+/**
+ * @brief Obrada događaja tipkala
+ * 
+ * Upravlja LED diodom ovisno o pritisnutom tipkalu
+ */
 void handleButtonEvents() {
   // Obrada INT0 događaja
   if (buttonPressed[0]) {
@@ -130,6 +208,9 @@ void handleButtonEvents() {
     while (serialInUse);
     serialInUse = true;
     Serial.println("Obrada INT0 događaja");
+    digitalWrite(debug_Serial, HIGH);
+    delayMicroseconds(100);
+    digitalWrite(debug_Serial, LOW);
     serialInUse = false;
   }
   
@@ -139,6 +220,9 @@ void handleButtonEvents() {
     while (serialInUse);
     serialInUse = true;
     Serial.println("Obrada INT1 događaja");
+    digitalWrite(debug_Serial, HIGH);
+    delayMicroseconds(100);
+    digitalWrite(debug_Serial, LOW);
     serialInUse = false;
   }
   
@@ -148,13 +232,18 @@ void handleButtonEvents() {
     while (serialInUse);
     serialInUse = true;
     Serial.println("Obrada INT5 događaja");
+    digitalWrite(debug_Serial, HIGH);
+    delayMicroseconds(100);
+    digitalWrite(debug_Serial, LOW);
     serialInUse = false;
     
     // Blinkanje LED nakon INT5
     for (int i = 0; i < 3; i++) {
       digitalWrite(ledPin, HIGH);
+      digitalWrite(debug_LED, HIGH);
       delay(200);
       digitalWrite(ledPin, LOW);
+      digitalWrite(debug_LED, LOW);
       delay(200);
     }
     ledState = false;
@@ -163,10 +252,16 @@ void handleButtonEvents() {
   // Gašenje LED ako nije aktivna obrada
   if (!buttonPressed[0] && !buttonPressed[1] && !buttonPressed[2] && ledState) {
     digitalWrite(ledPin, LOW);
+    digitalWrite(debug_LED, LOW);
     ledState = false;
   }
 }
 
+/**
+ * @brief Obrada očitanja senzora
+ * 
+ * Ispisuje vrijednost potenciometra na serijski monitor
+ */
 void handleSensorReading() {
   if (timerFlag) {
     timerFlag = false;
@@ -174,10 +269,18 @@ void handleSensorReading() {
     serialInUse = true;
     Serial.print("Vrijednost senzora: ");
     Serial.println(sensorValue);
+    digitalWrite(debug_Serial, HIGH);
+    delayMicroseconds(100);
+    digitalWrite(debug_Serial, LOW);
     serialInUse = false;
   }
 }
 
+/**
+ * @brief Normalan rad sustava
+ * 
+ * Ispisuje statusnu poruku svake 2 sekunde
+ */
 void normalOperation() {
   static unsigned long lastPrint = 0;
   if (millis() - lastPrint > 2000) {
@@ -185,6 +288,9 @@ void normalOperation() {
     while (serialInUse);
     serialInUse = true;
     Serial.println("Normalan rad sustava...");
+    digitalWrite(debug_Serial, HIGH);
+    delayMicroseconds(100);
+    digitalWrite(debug_Serial, LOW);
     serialInUse = false;
   }
 }
